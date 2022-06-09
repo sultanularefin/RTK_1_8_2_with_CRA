430b2c25e072d1192d9bcf111ec2de29fcbf4c4c
sultanularefin <mhmdarefin@gmail.com>
2022-06-09 16:24:10 +0600

    jsst test need to check

---
+++ src/test/whiteboard.test.js
@@ -0,0 +1,83 @@
+// import { Whiteboard } from "../src/whiteboard";
+
+// import {Whiteboard} from "../src/whiteboard.js";
+
+import {Whiteboard} from "../white_Board/Whiteboard";
+
+describe("Whiteboard contstructor", () => {
+    test("items can be ordered, does not throw exception", () => {
+        const items = [
+            {
+                id: 'dog',
+                lowerId: undefined,
+                data: 'hello' },
+            {
+                id: 'hamster',
+                lowerId: 'cat',
+                data: 'hello' },
+            {
+                id: 'cat',
+                lowerId: 'rabbit',
+                data: 'hello' },
+            {
+                id: 'rabbit',
+                lowerId: 'dog',
+                data: 'hello'
+            }
+        ];
+
+        new Whiteboard(items);
+    });
+
+    test("no items, does not throw exception", () => {
+        new Whiteboard([]);
+    });
+
+    test("no bottom item, throws exception", () => {
+        const items = [
+            {
+                id: 'dog',
+                lowerId: 'zzz',
+                data: 'hello' },
+            { id: 'hamster',
+                lowerId: 'cat',
+                data: 'hello' },
+            { id: 'cat',
+                lowerId: 'rabbit',
+                data: 'hello' },
+            { id: 'rabbit',
+                lowerId: 'dog',
+                data: 'hello'
+            }
+        ];
+
+        expect(() => { new Whiteboard(items) }).toThrow();
+    });
+
+    test("items cannot be ordered, throws exception", () => {
+        const items = [
+            { id: 'dog',
+                lowerId: undefined,
+                data: 'hello' },
+            { id: 'hamster',
+                lowerId: 'cat',
+                data: 'hello' },
+            { id: 'cat',
+                lowerId: 'rabbit',
+                data: 'hello'
+            },
+            {
+                id: 'rabbit',
+                lowerId: 'zzz',
+                data: 'hello'
+            }
+        ];
+
+        expect(() => {new Whiteboard(items)}).toThrow();
+    });
+});
+
+
+/*
+
+*/
